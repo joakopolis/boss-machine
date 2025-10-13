370b7beb2fc0cce5646afffc6fc3c453d21972b6
joakopolis <jmoreno.arg@gmail.com>
2025-10-13 17:47:49 -0300

    start project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+module.exports = app;
+
+// Add middleware for handling CORS requests from index.html
+app.use(cors());
+
+// Add middware for parsing request bodies here:
+app.use(bodyParser.json())
+// Mount your existing apiRouter below at the '/api' path.
+const apiRouter = require('./server/api');
+
+apiRouter.use('/', (req, res, next) => {
+    console.log('path accessed');
+    next();
+})
